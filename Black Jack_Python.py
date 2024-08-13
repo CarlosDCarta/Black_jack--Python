76f73eae3f02d9c703d35d191fbfdd8353c39cf8
Carlosgit config --global user.email <Carlos>
2024-08-13 15:56:04 -0400

    added compare function and cleaned up calculations

--- Black Jack_Python.py
+++ Black Jack_Python.py
@@ -1,20 +1,22 @@
 import Art, random
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+print(Art.logo1)
 
 def assigning_cards():
     user = []
     computer = []
     user = random.sample(cards,2)
     computer = random.sample(cards, 2)
-    print(f"Your cards are {user}\nThe computers cards are {computer}")
-
     return user, computer
  
 def calculations(user_cards, computer_cards):
     sum_user = sum(user_cards)
     sum_computer = sum(computer_cards)
-    print(sum_user,sum_computer)
+    if 11 in user_cards and sum_user > 21:
+        user_cards.remove(11)
+        user_cards.append(1)
+    return sum_user, sum_computer
 
 def continue_playing(choice):
     if choice == 'n':
@@ -22,7 +24,16 @@ def continue_playing(choice):
         return play_game
     else:
         return True
-#def user_hit_or_stand(choice):
+    
+
+def compare(sum_user, sum_computer):
+    if sum_user == 21:
+        print("You Win")
+    elif sum_computer == 21:
+        print("You Lose")
+    else:
+        return True
+
 
 
 
@@ -32,7 +43,10 @@ if choice == 'y':
 
 while play_game:
     user_cards, computer_cards = assigning_cards()
-    calculations(user_cards, computer_cards)
+    sum_user, sum_computer = calculations(user_cards, computer_cards)
+    compare(sum_user,sum_computer)
+    print(f"Your cards are {user_cards}. Your score is {sum_user}")
+    print(f"The computers first card is {computer_cards[0]}")
     
     choice = str(input("Type 'y' to continue or type 'n' to stop\n")).lower()
     play_game = continue_playing(choice)
